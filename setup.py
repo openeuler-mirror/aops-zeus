a2173cb4a514cd7a0869fb207746e4833f56cc89
zhu-yuncheng <zhuyuncheng@huawei.com>
2022-09-29 16:50:55 +0800

    init zeus

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Description: setup up the A-ops manager service.
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='aops-zeus',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'ansible>=2.9.0',
+        'PyYAML',
+        'marshmallow>=3.13.0',
+        'Flask',
+        'Flask-RESTful',
+        'requests',
+        'SQLAlchemy',
+        'Werkzeug'
+        ],
+    author='cmd-lsw-yyy-zyc',
+    data_files=[
+        ('/etc/aops', ['conf/zeus.ini', 'conf/default.json']),
+        ('/usr/lib/systemd/system', ['aops-zeus.service']),
+    ],
+    scripts=['aops-zeus'],
+    zip_safe=False
+)
