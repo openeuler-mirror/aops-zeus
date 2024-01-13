34a2caf0cee69caa4e3f32a1d3a745a9067082ef
Hu gang <18768366022@163.com>
2024-01-13 17:00:25 +0800

    feat: zeus support socket connection for host terminal

--- zeus/manage.py
+++ zeus/manage.py
@@ -26,8 +26,16 @@ from vulcanus import init_application
 from zeus.conf import configuration
 from zeus.url import URLS
 
+from zeus.host_manager.terminal import socketio
+
+
 app = init_application(name="zeus", settings=configuration, register_urls=URLS)
 
+socketio.init_app(app=app)
 
 if __name__ == "__main__":
-    app.run(host=configuration.zeus.get('IP'), port=configuration.zeus.get('PORT'))
+    socketio.run(
+        app,
+        host=configuration.zeus.get("IP"),
+        port=configuration.zeus.get("PORT"),
+    )
