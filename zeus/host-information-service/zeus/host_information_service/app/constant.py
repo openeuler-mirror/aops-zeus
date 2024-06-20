04c61ed402faeba6998bcba7efb131cfc004bca6
gongzt <gong_zhengtang@163.com>
2024-06-20 15:09:26 +0800

    主机微服务api初始化，包含主机组和主机的业务处理逻辑

---
+++ zeus/host-information-service/zeus/host_information_service/app/constant.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# ******************************************************************************
+# Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+# licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN 'AS IS' BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# ******************************************************************************/
+
+
+# host template file content
+HOST_TEMPLATE_FILE_CONTENT = """host_ip,ssh_port,ssh_user,password,ssh_pkey,host_name,host_group_name,management
+127.0.0.1,22,root,password,private key,test_host,test_host_group,FALSE
+127.0.0.1,23,root,password,private key,test_host,test_host_group,FALSE
+,,,,,,,
+"提示:",,,,,,,
+"1. 除登录密码与SSH登录秘钥外,其余信息都应提供有效值",,,,,,,
+"2. 登录密码与SSH登录秘钥可选择一种填入,当两者都提供时,以SSH登录秘钥为准",,,,,,,
+"3. 添加的主机信息不应存在重复信息(主机名称重复或者主机IP+端口重复)",,,,,,,
+"4. 上传本文件前,请删除此部分提示内容",,,,,,,
+"""
+
+
+class HostStatus:
+    ONLINE = 0
+    OFFLINE = 1
+    UNESTABLISHED = 2
+    SCANNING = 3
---
+++ zeus/host-information-service/zeus/host_information_service/database/table.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+# ******************************************************************************
+# Copyright (c) Huawei Technologies Co., Ltd. 2021-2023. All rights reserved.
+# licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN 'AS IS' BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# ******************************************************************************/
+from sqlalchemy import Column
+from sqlalchemy.sql.sqltypes import Boolean, Integer, String
+from vulcanus.database import Base
+
+
+class Host(Base):  # pylint: disable=R0903
+    """
+    Host table
+    """
+
+    __tablename__ = "host"
+
+    host_id = Column(String(36), primary_key=True)
+    host_name = Column(String(50), nullable=False)
+    host_ip = Column(String(16), nullable=False)
+    management = Column(Boolean, nullable=False)
+    host_group_name = Column(String(20))
+    repo_id = Column(String(36))
+    last_scan = Column(Integer)
+    scene = Column(String(255))
+    os_version = Column(String(40))
+    ssh_user = Column(String(40), default="root")
+    ssh_port = Column(Integer(), default=22)
+    pkey = Column(String(4096))
+    status = Column(Integer(), default=2)
+    reboot = Column(Boolean, nullable=False, default=False)
+    host_group_id = Column(String(36))
+    cluster_id = Column(String(36))
+    ext_props = Column(String(1024))
+
+
+class HostGroup(Base):
+    """
+    Host group table
+    """
+
+    __tablename__ = "host_group"
+
+    host_group_id = Column(String(36), primary_key=True)
+    host_group_name = Column(String(20))
+    cluster_id = Column(String(36))
+    description = Column(String(60))
+
+
+class Cluster(Base):
+    """
+    Cluster table
+    """
+
+    __tablename__ = "cluster"
+
+    cluster_id = Column(String(36), primary_key=True)
+    cluster_name = Column(String(20))
+    description = Column(String(60))
+    subcluster = Column(Boolean, default=False)
+    backend_ip = Column(String(16))
+    public_key = Column(String(4096))
+    private_key = Column(String(4096))
+    synchronous_state = Column(String(20))
